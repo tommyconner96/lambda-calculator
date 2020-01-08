7156000a29eac1c35df6f6e38bce789a4f9ba8b9
unknown <tommyconner96@gmail.com>
2020-01-08 08:01:00 -0500

    mvp

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
-};
+const NumberButton = props => {
+
+  return  <button className="numberButtons">{props.numberString}</button>
+
+  
+}
+
+export default NumberButton
--- src/data.js
+++ src/data.js
@@ -30,3 +30,6 @@ const operators = [
 ];
 
 const specials = ["C", "+/-", "%"];
+
+
+export {numbers, operators, specials}
